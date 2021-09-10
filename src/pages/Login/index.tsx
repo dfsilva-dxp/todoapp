e3e83959d9ddfa9dfe988c41f3a383633e0152f7
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-10 18:48:13 -0300

    feat: implementando o form de signIn e signUp

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,53 @@
+import { useState } from "react";
+import { TiThListOutline } from "react-icons/ti";
+
+import * as S from "./styles";
+
 export const Login = () => {
-  return <h1>Login</h1>;
+  const [hasAccount, setHasAccount] = useState(false);
+
+  function handleClick() {
+    setHasAccount(!hasAccount);
+  }
+
+  return (
+    <S.Main>
+      <S.Container>
+        <S.Wrap>
+          <S.Form>
+            <S.Title data-title="Todo App">
+              <TiThListOutline />
+            </S.Title>
+            <S.Input type="email" name="email" required placeholder="E-mail" />
+            <S.Input
+              type="password"
+              name="password"
+              required
+              placeholder="Senha"
+            />
+            {!hasAccount ? (
+              <>
+                <S.Button type="submit">Entrar</S.Button>
+              </>
+            ) : (
+              <>
+                <S.Button type="submit">Cadastrar</S.Button>
+              </>
+            )}
+
+            {!hasAccount ? (
+              <p>
+                Não tem uma conta?{" "}
+                <span onClick={handleClick}>Clique aqui</span>
+              </p>
+            ) : (
+              <p>
+                Já tem uma conta? <span onClick={handleClick}>Clique aqui</span>
+              </p>
+            )}
+          </S.Form>
+        </S.Wrap>
+      </S.Container>
+    </S.Main>
+  );
 };
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,1 +1,120 @@
-import styles from "styled-components";
+import styled from "styled-components";
+
+export const Main = styled.main`
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 1rem;
+
+  @media (min-width: 576px) {
+    & {
+      max-width: 540px;
+    }
+  }
+  @media (min-width: 768px) {
+    & {
+      max-width: 720px;
+    }
+  }
+  @media (min-width: 992px) {
+    & {
+      max-width: 960px;
+    }
+  }
+  @media (min-width: 1200px) {
+    & {
+      max-width: 1140px;
+    }
+  }
+`;
+
+export const Wrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+
+export const Form = styled.form`
+  width: min(420px, 100%);
+  background: #fff;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 60px rgba(0, 0, 0, 0.05);
+
+  display: grid;
+  grid-template-rows: repeat(5, 3rem);
+  gap: 1rem;
+
+  p {
+    text-align: center;
+    color: #777;
+
+    span {
+      font-weight: 600;
+      text-decoration: underline;
+      cursor: pointer;
+      transition: color 0.15s;
+
+      &:hover {
+        color: #fd6699;
+      }
+    }
+  }
+`;
+
+export const Title = styled.h4`
+  font-size: 2.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::after {
+    content: attr(data-title);
+    margin-left: 0.5rem;
+  }
+`;
+
+export const Input = styled.input`
+  display: block;
+  width: 100%;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+
+  &::placeholder {
+    font-weight: 600;
+  }
+`;
+
+export const Button = styled.button`
+  display: inline-block;
+  padding: 0.375rem 0.75rem;
+  text-align: center;
+  vertical-align: middle;
+  user-select: none;
+  background-color: transparent;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  font-weight: 600;
+  color: #fff;
+  background-color: #fd6699;
+  border-color: #fd6699;
+
+  transition: filter 0.15s ease-in-out;
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+`;
