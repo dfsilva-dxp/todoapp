4c6be736decb6a1654e694d54e3f4bd6d30a14e3
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-12 13:16:17 -0300

    feat: removendo estilos

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -79,23 +79,6 @@ export const Title = styled.h4`
   }
 `;
 
-export const Input = styled.input`
-  display: block;
-  width: 100%;
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  color: #495057;
-  background-color: #fff;
-  background-clip: padding-box;
-  border: 1px solid #ced4da;
-  border-radius: 0.25rem;
-
-  &::placeholder {
-    font-weight: 600;
-  }
-`;
-
 export const Button = styled.button`
   display: inline-block;
   padding: 0.375rem 0.75rem;
