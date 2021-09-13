addd6f7e124003192033c4420b2214eebf1179e9
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-13 17:37:21 -0300

    chore: firebase config

---
+++ src/services/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import authentication from "firebase/auth";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
+firebase.initializeApp(config);
+
+export { authentication };
+export default firebase;
