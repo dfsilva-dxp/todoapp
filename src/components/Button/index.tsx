cfb4b137dc6c575ae7572204d282f952c91beb63
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-14 18:50:53 -0300

    feat: recebendo a função handleSignIn

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,16 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
+import { ButtonHTMLAttributes, FormEvent, ReactNode } from "react";
 
 import * as S from "./styles";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  handleSignIn?: (event: FormEvent<HTMLButtonElement>) => void;
 }
 
-export function Button({ children, ...props }: ButtonProps) {
-  return <S.Button {...props}>{children}</S.Button>;
+export function Button({ children, handleSignIn, ...props }: ButtonProps) {
+  return (
+    <S.Button onClick={handleSignIn} {...props}>
+      {children}
+    </S.Button>
+  );
 }
