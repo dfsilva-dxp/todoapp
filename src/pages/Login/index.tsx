12b8b6803d5abba39f518e9f439ac88bd2edbf2b
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-14 18:53:11 -0300

    feat: onChange dos inputs e testando a função signIn

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,35 +1,65 @@
+import { FormEvent, useState } from "react";
+import { TiThListOutline } from "react-icons/ti";
+
 import { Button } from "components/Button";
 import { Input } from "components/Input";
-import { useState } from "react";
-import { TiThListOutline } from "react-icons/ti";
+
+import { useAuth } from "hooks/useAuth";
 
 import * as S from "./styles";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [hasAccount, setHasAccount] = useState(false);
+
+  const { signIn } = useAuth();
+
+  function setCredentials() {
+    setEmail("");
+    setPassword("");
+  }
 
   function handleClick() {
     setHasAccount(!hasAccount);
+  }
+
+  function handleSignIn(e: FormEvent) {
+    e.preventDefault();
+    const credentials = { email, password };
+    signIn(credentials);
+    setCredentials();
   }
 
   return (
     <S.Main>
       <S.Container>
         <S.Wrap>
-          <S.Form>
+          <S.Form onSubmit={(e) => e.preventDefault()}>
             <S.Title data-title="Todo App">
               <TiThListOutline />
             </S.Title>
-            <Input type="email" name="email" required placeholder="E-mail" />
+            <Input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="E-mail"
+            />
             <Input
               type="password"
               name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               required
               placeholder="Senha"
             />
             {!hasAccount ? (
               <>
-                <Button type="submit">Entrar</Button>
+                <Button type="submit" handleSignIn={handleSignIn}>
+                  Entrar
+                </Button>
               </>
             ) : (
               <>
