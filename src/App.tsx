b79d06a14153e22766e88f078f70b1c0df03daa3
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-14 18:52:12 -0300

    feat: context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { AuthContextProvider } from "contexts/AuthContext";
 import { Login } from "./pages/Login";
+
 import { GlobalStyles } from "./styles/global";
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <GlobalStyles />
       <Login />
-    </>
+    </AuthContextProvider>
   );
 }
 
