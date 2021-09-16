749564952bc522b8cf6b90450ca65d3fec418f25
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-16 15:16:37 -0300

    feat: recebendo os dados de user e loading

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,20 @@
+import { useAuth } from "../../hooks/useAuth";
+
 export function Home() {
-  return <h1>Home</h1>;
+  const { user, loading, logout } = useAuth();
+
+  return (
+    <>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <>
+          <h1>{user.email}</h1>
+          <button type="button" onClick={logout}>
+            Logout
+          </button>
+        </>
+      )}
+    </>
+  );
 }
