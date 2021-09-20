6df1bb0d68f1273996c1296fa3189c9b04332cd6
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-20 18:59:11 -0300

    feat: removendo o handleSubmit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,11 @@
-import { ButtonHTMLAttributes, FormEvent, ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
 import * as S from "./styles";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  handleSignIn?: (event: FormEvent<HTMLButtonElement>) => void;
 }
 
-export function Button({ children, handleSignIn, ...props }: ButtonProps) {
-  return (
-    <S.Button onClick={handleSignIn} {...props}>
-      {children}
-    </S.Button>
-  );
+export function Button({ children, ...props }: ButtonProps) {
+  return <S.Button {...props}>{children}</S.Button>;
 }
