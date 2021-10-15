84c492b33022aa1e69ca2a45bf2a47a44ae77fcf
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-15 18:40:41 -0300

    feat: page login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,131 +1,18 @@
-// import { ChangeEvent, FormEvent, useCallback, useState } from "react";
-
-// import { useAuth } from "../../hooks/useAuth";
-
-// import { Button } from "../../components/Button";
-// import { Input } from "../../components/Input";
+import { LoginRoutes } from "../../routes/login";
 
 import * as S from "./styles";
-// import { useHistory } from "react-router";
-
-// type Credentials = {
-//   email: string;
-//   password: string;
-// };
 
 export const Login = () => {
-  // const [credentials, setCredentials] = useState<Credentials>({
-  //   email: "",
-  //   password: "",
-  // });
-  // const [hasAccount, setHasAccount] = useState(false);
-  // const { signIn, signOut } = useAuth();
-  // const history = useHistory();
-
-  // const toggleHasAccount = () => {
-  //   setHasAccount(!hasAccount);
-  // };
-
-  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   const { name, value } = e.target;
-  //   setCredentials({
-  //     ...credentials,
-  //     [name]: value,
-  //   });
-  // };
-
-  // const handleSignIn = useCallback(
-  //   (e: FormEvent) => {
-  //     e.preventDefault();
-
-  //     signIn(credentials);
-  //     history.push("/");
-
-  //     setCredentials({
-  //       email: "",
-  //       password: "",
-  //     });
-  //   },
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  //   [credentials]
-  // );
-
-  // const handleSignOut = useCallback(
-  //   async (e: FormEvent) => {
-  //     e.preventDefault();
-
-  //     await signOut(credentials)
-  //       .then(() => {
-  //         setCredentials({
-  //           email: "",
-  //           password: "",
-  //         });
-  //       })
-  //       .finally(() => setHasAccount(false));
-  //   },
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  //   [credentials]
-  // );
-
   return (
     <S.Main>
       <S.Container>
-        <S.Wrap>
-          {/* <S.Form onSubmit={(e) => e.preventDefault()}>
-            {!hasAccount ? (
-              <S.Title data-title="Login" />
-            ) : (
-              <S.Title data-title="Cadastro" />
-            )}
-
-            <Input
-              type="email"
-              name="email"
-              label="Seu e-mail"
-              id="email"
-              value={credentials.email}
-              onChange={handleChange}
-              required
-              placeholder="johndoe@email.com"
-            />
-            <Input
-              type="password"
-              name="password"
-              label="Sua senha"
-              id="password"
-              value={credentials.password}
-              onChange={handleChange}
-              required
-              placeholder="Senha"
-            />
-            {!hasAccount ? (
-              <>
-                <Button type="submit" onClick={handleSignIn}>
-                  Entrar
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button type="submit" onClick={handleSignOut}>
-                  Cadastrar
-                </Button>
-              </>
-            )}
-
-            {!hasAccount ? (
-              <p>
-                Não tem uma conta?{" "}
-                <span onClick={toggleHasAccount}>Clique aqui</span>
-              </p>
-            ) : (
-              <p>
-                Já tem uma conta?{" "}
-                <span onClick={toggleHasAccount}>Clique aqui</span>
-              </p>
-            )}
-          </S.Form> */}
-        </S.Wrap>
+        <S.Content>
+          <LoginRoutes />
+        </S.Content>
       </S.Container>
+      <S.Footer>
+        <p>&copy; 2021 &bull; Alguns direitos reservados</p>
+      </S.Footer>
     </S.Main>
   );
 };
