1803ed2f057e14078cc14368844a8af7e90a4cc4
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-21 11:23:42 -0300

    feat: import sendResetPassword form

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -2,6 +2,7 @@ import { Route, Switch, useRouteMatch } from "react-router-dom";
 
 import { SignInForm } from "../components/SignInForm";
 import { SignOutForm } from "../components/SignOutForm";
+import { SendResetPassword } from "../components/SendResetPassword";
 
 export const LoginRoutes = () => {
   const { path } = useRouteMatch();
@@ -10,6 +11,7 @@ export const LoginRoutes = () => {
     <Switch>
       <Route exact path={path} component={SignInForm} />
       <Route path={`${path}/cadastrar`} component={SignOutForm} />
+      <Route path={`${path}/esqueci-a-senha`} component={SendResetPassword} />
     </Switch>
   );
 };
