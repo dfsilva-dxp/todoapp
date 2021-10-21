ba9e46007e0fa0fc710ace347d8c31dcc7535eff
Daniel Silva <dani3l.dxp@gmail.com>
2021-10-21 09:46:19 -0300

    feat: finalizando o socialButton

--- src/components/SocialButton/index.tsx
+++ src/components/SocialButton/index.tsx
@@ -4,8 +4,23 @@ import * as S from "./styles";
 
 interface SocialButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: string | JSX.Element;
+  backgroundColor: string;
+  iconColor: string;
 }
 
-export function SocialButton({ children, ...props }: SocialButtonProps) {
-  return <S.Button {...props}>{children}</S.Button>;
+export function SocialButton({
+  children,
+  backgroundColor,
+  iconColor,
+  ...props
+}: SocialButtonProps) {
+  return (
+    <S.Button
+      backgroundColor={backgroundColor}
+      iconColor={iconColor}
+      {...props}
+    >
+      {children}
+    </S.Button>
+  );
 }
--- src/components/SocialButton/styles.ts
+++ src/components/SocialButton/styles.ts
@@ -1,3 +1,35 @@
 import styled from "styled-components";
 
-export const Button = styled.button``;
+interface ButtonProps {
+  backgroundColor: string;
+  iconColor: string;
+}
+
+export const Button = styled.button<ButtonProps>`
+  min-width: 3rem;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  padding: 0.75rem 1.25rem;
+  vertical-align: middle;
+  user-select: none;
+  border: 1px solid var(--gray-150);
+  border-radius: 0.25rem;
+  font-size: 0.875rem;
+  color: ${({ iconColor }) => iconColor};
+  line-height: 1.5;
+  font-weight: 500;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  transition: filter 0.2s;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    font-size: 1.25rem;
+  }
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+`;
